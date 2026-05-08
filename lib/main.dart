7ced6e4324b5f39d9bcc0d196144e8d818accf31
azrulfattah336 <azrulfatah336@gmail.com>
2026-05-08 12:43:14 +0700

    Menambah tampilan bertema gelap

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Hadist Arbain',
+
+      theme: ThemeData.dark(),
+      
       home: const HomeScreen(),
     );
   }
