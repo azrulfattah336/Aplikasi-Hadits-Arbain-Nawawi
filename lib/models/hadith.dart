78de552a43602527cc4214cf431534d83ffbd4f3
azrulfattah336 <azrulfatah336@gmail.com>
2026-05-09 20:14:24 +0700

    Menambah model hadist

---
+++ lib/models/hadith.dart
@@ -0,0 +1,15 @@
+class Hadith {
+  final int id;
+  final String title;
+  final String arab;
+  final String translation;
+  final String explanation;
+
+  Hadith({
+    required this.id,
+    required this.title,
+    required this.arab,
+    required this.translation,
+    required this.explanation,
+  });
+}
