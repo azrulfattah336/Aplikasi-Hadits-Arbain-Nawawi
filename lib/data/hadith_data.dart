9c70b045e236b3be69761d46cd79ceeb18112847
azrulfattah336 <azrulfatah336@gmail.com>
2026-05-09 20:25:11 +0700

    menambah data hadist awal

---
+++ lib/data/hadith_data.dart
@@ -0,0 +1,19 @@
+import '../models/hadith.dart';
+
+final List<Hadith> hadithList = [
+  Hadith(
+    id: 1,
+    title: "Hadits 1",
+    arab: "إِنَّمَا الْأَعْمَالُ بِالنِّيَّاتِ",
+    translation: "Sesungguhnya amal itu tergantung niatnya",
+    explanation: "Hadits ini menjelaskan pentingnya niat.",
+  ),
+
+  Hadith(
+    id: 2,
+    title: "Hadits 2",
+    arab: "بُنِيَ الإِسْلاَمُ عَلَى خَمْسٍ",
+    translation: "Islam dibangun atas lima perkara",
+    explanation: "Hadits ini menjelaskan rukun Islam.",
+  ),
+];
