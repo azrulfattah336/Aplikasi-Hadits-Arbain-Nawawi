a87da868c9482bf9f6d9df82e2d78acb1458c789
azrulfattah336 <azrulfatah336@gmail.com>
2026-05-10 19:11:08 +0700

    Menambahkan fitur search

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,25 +1,62 @@
 import 'package:flutter/material.dart';
 import '../data/hadith_data.dart';
-import '../widgets/hadith_card.dart';
+import '../wigdets/hadith_card.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+   State<HomeScreen> createState() => _HomeScreenState();
+    }
+
+   class _HomeScreenState extends State<HomeScreen> {
+
+  String searchQuery = "";
+
+  @override
   Widget build(BuildContext context) {
+    final filtered = hadithList.where((h) {
+       return h.title
+      .toLowerCase()
+      .contains(searchQuery.toLowerCase());
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: const Text("Hadits Arbain Nawawi"),
       ),
 
-      body: ListView.builder(
-        itemCount: hadithList.length,
-        itemBuilder: (context, index) {
-          return HadithCard(
-            hadith: hadithList[index],
-          );
-        },
+      body: Column(
+        children: [
+
+         Padding(
+           padding: const EdgeInsets.all(10),
+           child: TextField(
+           decoration: const InputDecoration(
+           hintText: "Cari hadits...",
+           border: OutlineInputBorder(),
+           prefixIcon: Icon(Icons.search),
+           ),
+
+           onChanged: (value) {
+            setState(() {
+              searchQuery = value;
+            });
+          },
+        ),
       ),
+
+      Expanded(
+        child: ListView.builder(
+          itemCount: filtered.length,
+          itemBuilder: (context, index) {
+            return HadithCard(
+              hadith: filtered[index],
+            );
+          },
+        ),
+      ),
+    ],
+  ),
     );
   }
 }
